03e091c2a48ed13b9be66b8baca8683ae4670aa4
mrezav <muhammadreza.dev@gmail.com>
2020-05-27 15:09:53 +0700

    config and login

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class HomeController extends Controller
+{
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(!$user){
+            return response([
+                'message' => 'Maaf email tidak terdaftar!'
+            ], 404);
+        }else if(!Hash::check($request->password, $user->password)){
+            return response([
+                'message' => 'Password salah!'
+            ], 422);
+        }
+        $token = $user->createToken('user-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+            'isLogin' => true
+        ];
+
+        return response($response, 200);
+    }
+
+    public function register(Request $request){
+        $validate = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5'
+        ]);
+
+        if($validate->fails()){
+           return response([
+               'message' => $validate->errors()
+           ], 402);
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response($request, 201);
+    }
+
+    public function users(){
+        $user = User::all();
+        return response([
+            'user' => $user
+        ], 200);
+    }
+
+    public function logout(Request $request){
+        $user = User::where('email', $request->email)->first();
+        $user->tokens()->delete();
+        return response([
+            'message' => 'Logout Berhasil!',
+            'status' => true
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/users', 'HomeController@users');
+});
+
+Route::post('login', 'HomeController@login');
+Route::post('register', 'HomeController@register');
+Route::get('logout', 'HomeController@logout');
